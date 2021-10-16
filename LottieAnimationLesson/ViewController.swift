eb76f3af5d1e51dd8ac99ce48a31c89c2c7c18be
HiroUrata <1712026@i.softbank.jp>
2021-10-16 19:26:58 +0900

    一時中断　showLottieAnimation 編集中

--- LottieAnimationLesson/ViewController.swift
+++ LottieAnimationLesson/ViewController.swift
@@ -6,9 +6,10 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
@@ -32,11 +33,48 @@ class ViewController: UIViewController {
             button.layer.shadowOffset = CGSize(width: 5, height: 5)
             button.layer.shadowRadius = 5.0
             button.layer.shadowOpacity = 0.8
+            button.addTarget(self, action: #selector(showLottie), for: .touchDown)
             return button
         }()
         view.addSubview(animationStartButton)
+        
     }
-
+    
+    @objc func showLottie(sender:UIButton){
+        
+        let animationView = AnimationView()
+        animationView.showLottieAnimation(viewBackgroundColor: .clear, playAnimationName: "Lottie01", selectContentMode: .scaleToFill, selectLoopMode: .loop, animatoinTime: 5, targetView: view, afterProseccing:{
+            
+            sender.isEnabled = false
+        }())
+    }
 
 }
 
+extension AnimationView{
+    
+    func showLottieAnimation(viewBackgroundColor:UIColor,playAnimationName:String,selectContentMode:UIView.ContentMode,selectLoopMode:LottieLoopMode,animatoinTime: Double,targetView:UIView,afterProseccing: Void?){
+        
+        let lottieAnimationView = AnimationView()
+        
+        lottieAnimationView.frame = CGRect(x: targetView.frame.maxX / 6, y: targetView.frame.maxY / 4, width: targetView.frame.width - targetView.frame.maxX / 3, height: targetView.frame.width - targetView.frame.maxX / 3)
+        lottieAnimationView.backgroundColor = viewBackgroundColor
+        lottieAnimationView.animation = Animation.named(playAnimationName)
+        lottieAnimationView.contentMode = selectContentMode
+        lottieAnimationView.loopMode = selectLoopMode
+        lottieAnimationView.play()
+        targetView.addSubview(lottieAnimationView)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + animatoinTime) {
+            
+//            if afterProseccing != nil{
+                
+
+           // }
+            
+            lottieAnimationView.removeFromSuperview()
+            lottieAnimationView.stop()
+            afterProseccing
+        }
+    }
+}
